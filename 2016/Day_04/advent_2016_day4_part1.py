496ed26229fc97661ba47c9b6a1b346d165c4a74
flongy72 <45362924+flongy72@users.noreply.github.com>
2022-02-19 18:34:29 +0000

    update

--- 2016/Day_04/advent_2016_day4_part1.py
+++ 2016/Day_04/advent_2016_day4_part1.py
@@ -1,104 +1,83 @@
-
 def main():
-    total=0
-    checksum_list=[]
-    sector_id_list=[]
-    dict_list=[]
-    room_list=[]
-    filename = "advent_2016_day4.txt"
-    accessMode = "r"
-    my_file = open(filename, accessMode)
-    data=my_file.read()
-    list=data.split("\n")
-    for room in list:                                       # break the data string into 3 lists
-        checksum=""
-        sector_id=""
-        room_string=""
-        bracket1=room.index("[")
-        bracket2=room.index("]")
-        for i in range(bracket1+1,bracket2):
-            checksum += room[i]
-        checksum_list.append(checksum)                      # checksum list
-        for i in range(bracket1-3,bracket1):
-            sector_id += room[i]
-        sector_id=int(sector_id)
-        sector_id_list.append(sector_id)                    # sector_id list
-        for i in range(0,bracket1-4):
-            room_string += room[i]
-            new_room_string=room_string.replace("-","")
-        room_list.append(new_room_string)                   # room list
-    dummy=[]
-
-    for room in room_list:                                      # create a list of dictionaries
-        dict_list=[]                                            # which can be ordered and stil keep
-        for letter in room:                                     # the same data linked together
-            count=room.count(letter)
-            count=int(count)
-            dummy.append(count)
-            if {"letter":letter,"count":count} in dict_list:
+    total = 0
+    checksum_list = []
+    sector_id_list = []
+    room_list = []
+    with open("advent_2016_day4.txt") as input:
+        list = input.read().splitlines()
+        for room in list:
+            checksum = ""
+            sector_id = ""
+            room_string = ""
+            bracket1 = room.index("[")
+            bracket2 = room.index("]")
+            for i in range(bracket1 + 1, bracket2):
+                checksum += room[i]
+            checksum_list.append(checksum)
+            for i in range(bracket1 - 3, bracket1):
+                sector_id += room[i]
+            sector_id = int(sector_id)
+            sector_id_list.append(sector_id)
+            for i in range(0, bracket1 - 4):
+                room_string += room[i]
+                new_room_string = room_string.replace("-", "")
+            room_list.append(new_room_string)
+        dummy = []
+        for room in room_list:
+            dict_list = []
+            for letter in room:
+                count = room.count(letter)
+                count = int(count)
+                dummy.append(count)
+                if {"letter": letter, "count": count} in dict_list:
+                    continue
+                dict_list.append({"letter": letter, "count": count})
+            dict_list.sort(key=dict_sort_count)
+            v = len(dummy)
+            equal = True
+            for numbers in range(v):
+                if numbers > 0:
+                    if dummy[numbers] != dummy[numbers - 1]:
+                        equal = False  #
+            if equal:
+                dict_list.reverse()
+            dict_list.reverse()
+            letters = ""
+            counts = []
+            for i in range(len(dict_list)):
+                letter = dict_list[i]
+                letters += letter["letter"]
+                counts.append(letter["count"])
+            len2 = len(counts)
+            p = 0
+            for i in range(len2):
+                if i < p:
+                    continue
+                if i < (len2 - 1):
+                    if counts[i] == counts[i + 1]:
+                        number = counts.count(counts[i])
+                        p = i + number
+                        re_order_string = ""
+                        re_order_list = []
+                        new_string = ""
+                        for j in range(number):
+                            if (i + j) < (len2):
+                                re_order_list.append(letters[i + j])
+                                re_order_string += letters[i + j]
+                        re_order_list.sort()
+                        for q in re_order_list:
+                            new_string += q
+                        letters = letters.replace(re_order_string, new_string)
+            x = room_list.index(room)
+            checksum = checksum_list[x]
+            if letters.startswith(checksum):
+                total += sector_id_list[x]
                 continue
-            dict_list.append({"letter":letter,"count":count})
-
-        dict_list.sort(key=dict_sort_count)
-
-        v=len(dummy)                                            # this part was an afterthought
-        equal=True                                              # to avoid an anomoly if every count was equal
-        for numbers in range(v):                                # and the dictionary automatically
-            if numbers>0:                                       # put the order alphabetically
-                if dummy[numbers]!=dummy[numbers-1]:            # and so i needed to create
-                    equal=False                                 #
-        if equal:                                               #
-            dict_list.reverse()                                 # an extra reverse
-
-        dict_list.reverse()
-
-        letters=""
-        counts=[]
-        for i in range(len(dict_list)):                         # split the ordered dictionary
-            letter=dict_list[i]
-            letters += letter["letter"]                         # into letters
-            counts.append(letter["count"])                      # and counts of each letter
-
-        len2 = len(counts)
-        re_order_list=[]
-        re_order_string=""
-        new_string=""
-        number=0
-        p=0
-        for i in range(len2):                                               # arranging the letters list
-            if i < p:
-                continue
-            if i<(len2-1):                                                  # into high count to low count
-                if counts[i]==counts[i+1] :
-                    number=counts.count(counts[i])                          # and putting into alphabetical
-                    p=i+number
-                    re_order_string=""                                      # order if several letters had the
-                    re_order_list=[]
-                    new_string=""
-                    for j in range(number):                                 # same count
-                        if (i+j)<(len2):
-                            re_order_list.append(letters[i+j])
-                            re_order_string += letters[i+j]
-                    re_order_list.sort()
-                    for q in re_order_list:
-                        new_string += q
-                    letters=letters.replace(re_order_string,new_string)
-
-        x=room_list.index(room)
-        checksum=checksum_list[x]
-        if letters.startswith(checksum):
-            total += sector_id_list[x]
-            continue
-
-    print (total)
-
+        print(total)
 
 
 def dict_sort_count(e):
     return e["count"]
-
-
-
 
 
 if __name__ == '__main__':
--- 2016/Day_04/advent_2016_day4_part2.py
+++ 2016/Day_04/advent_2016_day4_part2.py
@@ -1,72 +1,64 @@
 
 
 def main():
-    total=0
+
+    def shift(x, y):
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        position = alphabet.index(x)
+        if y == 0:
+            return x
+        if y >= len(alphabet)-position:
+            new_position = position-(len(alphabet)-y)
+            return alphabet[new_position]
+        else:
+            new_position=position+y
+            return alphabet[new_position]
 
     checksum_list=[]
     sector_id_list=[]
-    dict_list=[]
     room_list=[]
     decrypted_list=[]
-    filename = "advent_2016_day4.txt"
-    accessMode = "r"
-    my_file = open(filename, accessMode)
-    data=my_file.read()
-    list=data.split("\n")
-    for room in list:                                       # break the data string into 3 lists
-        checksum=""
-        sector_id=""
-        room_string=""
-        bracket1=room.index("[")
-        bracket2=room.index("]")
-        for i in range(bracket1+1,bracket2):
-            checksum += room[i]
-        checksum_list.append(checksum)                      # checksum list
-        for i in range(bracket1-3,bracket1):
-            sector_id += room[i]
-        sector_id=int(sector_id)
-        sector_id_list.append(sector_id)                    # sector_id list
-        for i in range(0,bracket1-4):
-            room_string += room[i]
-
-        room_list.append(room_string)                   # room list
+    with open("advent_2016_day4.txt") as input:
+        for room in input.read().splitlines():
+            checksum = ""
+            sector_id = ""
+            room_string = ""
+            bracket1 = room.index("[")
+            bracket2 = room.index("]")
+            for i in range(bracket1+1, bracket2):
+                checksum += room[i]
+            checksum_list.append(checksum)
+            for i in range(bracket1-3, bracket1):
+                sector_id += room[i]
+            sector_id = int(sector_id)
+            sector_id_list.append(sector_id)
+            for i in range(0, bracket1-4):
+                room_string += room[i]
+            room_list.append(room_string)
 
 
+        for room in room_list:
+            decrypted=""
+            index=room_list.index(room)
+            forward=sector_id_list[index]
+            forward=float(forward)
+            x=forward/26
+            x=int(x)
+            move = forward % 26
+            move=int(move)
 
-    for room in room_list:
-        decrypted=""
-        index=room_list.index(room)
-        forward=sector_id_list[index]
-        forward=float(forward)
-        x=forward/26
-        x=int(x)
-        move = forward % 26
-        move=int(move)
+            for letter in room:
+                if letter=="-":
+                    decrypted += " "
+                    continue
 
-        for letter in room:
-            if letter=="-":
-                decrypted += " "
-                continue
+                new_letter = shift(letter,move)
+                decrypted += new_letter
+            decrypted_list.append(decrypted)
+            if decrypted=="northpole object storage":
+                print (forward)
 
-            new_letter=shift(letter,move)
-            decrypted += new_letter
-        decrypted_list.append(decrypted)
-        if decrypted=="northpole object storage":
-            print (forward)
-
-        print(decrypted)
-
-def shift(x,y):
-    alphabet="abcdefghijklmnopqrstuvwxyz"
-    position=alphabet.index(x)
-    if y==0:
-        return x
-    if y>=len(alphabet)-position:
-        new_position=position-(len(alphabet)-y)
-        return alphabet[new_position]
-    else:
-        new_position=position+y
-        return alphabet[new_position]
+            print(decrypted)
 
 
 if __name__ == '__main__':
