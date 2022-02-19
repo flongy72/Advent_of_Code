479b262355af3eb644bea0a6e8d0593dae8a35fc
flongy72 <45362924+flongy72@users.noreply.github.com>
2022-02-19 17:48:00 +0000

    update

--- 2016/Day_02/advent_2016_day2_part1.py
+++ 2016/Day_02/advent_2016_day2_part1.py
@@ -1,48 +1,26 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      furlo
-#
-# Created:     16/11/2021
-# Copyright:   (c) furlo 2021
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 def main():
-    pass
+    with open("advent_2016_day2.txt") as input:
+        x = 5
+        for line in input.read().splitlines():
+            for inst in line:
+                if inst == "U":
+                    if x in (4, 5, 6, 7, 8, 9):
+                        x -= 3
+                        continue
+                if inst == "D":
+                    if x in (1, 2, 3, 4, 5, 6):
+                        x += 3
+                    continue
+                if inst == "R":
+                    if x in (1, 2, 4, 5, 7, 8):
+                        x += 1
+                    continue
+                if inst == "L":
+                    if x in (2, 3, 5, 6, 8, 9):
+                        x -= 1
+                    continue
+            print(x)
+
 
 if __name__ == '__main__':
     main()
-filename = "advent_2016_day2.txt"
-accessMode = "r"
-myFile = open(filename, accessMode)
-line_list=[]
-x=5
-
-lines=myFile.read()
-new_list=lines.split("\n")
-for line in new_list:
-    for inst in line:
-
-        if inst=="U":
-            if x in (4,5,6,7,8,9):
-                x -= 3
-                continue
-        if inst=="D":
-            if x in (1,2,3,4,5,6):
-                x += 3
-            continue
-        if inst=="R":
-            if x in (1,2,4,5,7,8):
-                x += 1
-            continue
-        if inst=="L":
-            if x in (2,3,5,6,8,9):
-                x -= 1
-            continue
-    print(x)
-
-
-
-
--- 2016/Day_03/advent_2016_day3_part2.py
+++ 2016/Day_03/advent_2016_day3_part2.py
@@ -1,54 +1,24 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      furlo
-#
-# Created:     17/11/2021
-# Copyright:   (c) furlo 2021
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
 
 def main():
-    pass
+    with open("advent_2016_day3.txt") as input:
+        x = 0
+        new_list1 = []
+        new_list = [x.split() for x in input.read().splitlines()]
+        for i in range(3):
+            for each in new_list:
+                new_list1.append(int(each[i]))
+        b = 0
+        num = len(new_list1)
+        real_num = int(num / 3)
+        for a in range(real_num):
+            if (new_list1[b]) < (new_list1[b + 1]) + (new_list1[b + 2]) and (new_list1[b + 1]) < (new_list1[b]) + \
+                    (new_list1[b + 2]) and (new_list1[b + 2]) < (new_list1[b]) + (new_list1[b + 1]):
+                x += 1
+                b += 3
+            else:
+                b += 3
+        print(x)
+
 
 if __name__ == '__main__':
     main()
-filename = "advent_2016_day3.txt"
-accessMode = "r"
-my_File = open(filename, accessMode)
-data=my_File.read()
-x=0
-new_list=[]
-
-new_list1=[]
-triangles=data.splitlines()                         # whole program is for reading data in colums of 3 instead of line by line
-for each_triangle in triangles:
-    dimens=each_triangle.split()
-    new_list.append(dimens)
-
-for i in range(3):
-    for each in new_list:
-        new_list1.append(int(each[i]))
-
-b=0
-num=len(new_list1)
-
-real_num=int(num/3)
-
-for a in range(real_num):                           # iterating through the new list in groups of 3
-
-    if (new_list1[b])<(new_list1[b+1])+(new_list1[b+2]) and (new_list1[b+1])<(new_list1[b])+(new_list1[b+2]) and (new_list1[b+2])<(new_list1[b])+(new_list1[b+1]):
-        x +=1
-        b +=3
-    else:
-        b +=3
-
-print (x)
-
-
-
-
-
-
-
