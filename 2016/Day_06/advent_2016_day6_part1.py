f92b3c0a79b33606d68d8b5b9f9e120863a25f2e
flongy72 <45362924+flongy72@users.noreply.github.com>
2022-02-19 19:16:30 +0000

    update

--- 2016/Day_06/advent_2016_day6_part1.py
+++ 2016/Day_06/advent_2016_day6_part1.py
@@ -1,35 +1,30 @@
 
 
 def main():
+    def my_func(e):
+        return e["count"]
+    with open("advent_2016_day6.txt") as input:
+        list = input.read().splitlines()
+        new_list = [[] for _ in range(8)]
+        password = ""
+        for line in list:
+            length = len(line)
+            for i in range(length):
+                new_list[i] += line[i]
+        for new_line in new_list:
+            dict_list = []
+            letter_list = []
 
-    filename = "advent_2016_day6.txt"
-    accessMode = "r"
-    my_file = open(filename, accessMode)
-    data=my_file.read()
-    list=data.split("\n")
-    new_list=[[],[],[],[],[],[],[],[]]
-    password=""
-    for line in list:
-        length=len(line)
-        for i in range(length):
-            new_list[i] += line[i]
-    for new_line in new_list:
-        dict_list=[]
-        letter_list=[]
+            for letter in new_line:
+                if letter in letter_list:
+                    continue
+                count = new_line.count(letter)
+                dict_list.append({"letter": letter, "count": count})
+                letter_list.append(letter)
+            dict_list.sort(key=my_func)
+            password += dict_list[0]["letter"]   # change to index -1 for part 2 for least common character
+        print(password)
 
-        for letter in new_line:
-            if letter in letter_list:
-                continue
-            count=new_line.count(letter)
-            dict_list.append({"letter":letter,"count":count})
-            letter_list.append(letter)
-        dict_list.sort(key=my_func)
-                                     # this is removed for part 2
-        password += dict_list[0]["letter"]
-    print (password)
-
-def my_func(e):
-    return e["count"]
 
 if __name__ == '__main__':
     main()
