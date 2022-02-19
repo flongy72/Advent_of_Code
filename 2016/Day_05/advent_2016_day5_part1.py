65e438bba8cffd7c6134f7ef335adaf2322b439b
flongy72 <45362924+flongy72@users.noreply.github.com>
2022-02-19 19:07:30 +0000

    update

--- 2016/Day_05/advent_2016_day5_part1.py
+++ 2016/Day_05/advent_2016_day5_part1.py
@@ -1,27 +1,23 @@
-
 import hashlib
 
+
 def main():
-    i=0
-    x=0
-    password=""
-    while x<8:
-        i=str(i)
-        user = ("wtnhxymk" + i)
+    i = 0
+    x = 0
+    password = ""
+    while x < 8:
+        user = ("wtnhxymk" + str(i))
         h = hashlib.md5(user.encode())
         h2 = h.hexdigest()
-
-
         if h2.startswith("00000"):
-            print (i)
+            print(i)
             password += h2[5]
             x += 1
-            i = int(i)
             i += 1
             continue
-        i=int(i)
         i += 1
-    print (password)
+    print(password)
+
+
 if __name__ == '__main__':
     main()
-
--- 2016/Day_05/advent_2016_day5_part2.py
+++ 2016/Day_05/advent_2016_day5_part2.py
@@ -1,45 +1,36 @@
-
 import hashlib
 
+
 def main():
-    i=0
-    x=0
-    dict_list=[]
-    position_list=[]
-    password=""
-    while x<8:
-        i=str(i)
-        user = ("wtnhxymk" + i)
+    i = 0
+    x = 0
+    dict_list = []
+    position_list = []
+    password = ""
+    while x < 8:
+        user = ("wtnhxymk" + str(i))
         h = hashlib.md5(user.encode())
         h2 = h.hexdigest()
         if h2.startswith("00000"):
-            print (i)
-            position=h2[5]
-            character=h2[6]
-            if position.isdigit() and int(position)<8 and int(position) not in position_list:
-                position=int(position)
-                dict_list.append({"position":position,"character":character})
+            position = h2[5]
+            character = h2[6]
+            if position.isdigit() and int(position) < 8 and int(position) not in position_list:
+                position = int(position)
+                dict_list.append({"position": position, "character": character})
                 position_list.append(position)
                 x += 1
-                print(position_list)
-
-            i = int(i)
             i += 1
             continue
-        i=int(i)
         i += 1
-
     dict_list.sort(key=my_func)
-    print (dict_list)
-
     for i in dict_list:
-
         password += i["character"]
-    print (password)
+    print(password)
+
 
 def my_func(e):
     return e["position"]
 
+
 if __name__ == '__main__':
     main()
-
