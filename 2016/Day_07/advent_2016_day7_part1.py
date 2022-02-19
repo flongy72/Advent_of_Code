9a95150dbe86166838127569313cb61cdd6c370a
flongy72 <45362924+flongy72@users.noreply.github.com>
2022-02-19 19:33:41 +0000

    update

--- 2016/Day_07/advent_2016_day7_part1.py
+++ 2016/Day_07/advent_2016_day7_part1.py
@@ -1,36 +1,29 @@
 
-
 def main():
-
-    filename="advent_2016_day7.txt"
-    my_file=open(filename,"r")
-    data=my_file.read()
-
-    supports_tls=0
-
-    for line in data.split("\n"):
-        outside=True
-        outside_abba=False
-        inside_abba=False
-        i=0
-        while i<len(line)-4:
-            if line[i]==line[i+3] and line[i+1]==line[i+2] and outside:
-                outside_abba=True
-            if line[i]==line[i+3] and line[i+1]==line[i+2] and not outside:
-                inside_abba=True
-
-            if line[i]=="[":
-                outside=False
+    with open("advent_2016_day7.txt") as input:
+        supports_tls = 0
+        for line in input.read().splitlines():
+            outside = True
+            outside_abba = False
+            inside_abba = False
+            i = 0
+            while i < len(line)-4:
+                if line[i] == line[i+3] and line[i+1] == line[i+2] and outside:
+                    outside_abba = True
+                if line[i] == line[i+3] and line[i+1] == line[i+2] and not outside:
+                    inside_abba = True
+                if line[i] == "[":
+                    outside = False
+                    i += 1
+                    continue
+                if line[i] == "]":
+                    outside = True
+                    i += 1
+                    continue
                 i += 1
-                continue
-            if line[i]=="]":
-                outside=True
-                i += 1
-                continue
-            i += 1
-        if outside_abba and not inside_abba:
-            supports_tls += 1
-    print (supports_tls)
+            if outside_abba and not inside_abba:
+                supports_tls += 1
+        print(supports_tls)
 
 
 if __name__ == '__main__':
--- 2016/Day_07/advent_2016_day7_part2.py
+++ 2016/Day_07/advent_2016_day7_part2.py
@@ -1,52 +1,43 @@
 
 
 def main():
-
-    filename="advent_2016_day7.txt"
-    my_file=open(filename,"r")
-    data=my_file.read()
-    list=data.split("\n")
-    supports_ssl=0
-
-    for line in list:
-        outside=True
-        outside_aba=False
-        inside_bab=False
-
-        aba_list=[]
-        bab_list=[]
-        i=0
-        x=len(line)
-        while i<x-2:
-            aba=""
-            bab=""
-            if line[i]==line[i+2] and line[i+1]!=line[i] and outside:
-                aba += line[i] + line[i+1] + line[i+2]
-                if "[" not in aba and "]" not in aba:
-                    aba_list.append(aba)
-                if aba in bab_list:
-                    outside_aba=True
-
-            if line[i]==line[i+2] and line[i+1]!=line[i] and not outside:
-                bab += line[i+1] + line[i] + line[i+1]
-                if "[" not in bab and "]" not in bab:
-                    bab_list.append(bab)
-                if bab in aba_list:
-                    inside_bab=True
-
-            if line[i]=="[":
-                outside=False
+    with open("advent_2016_day7.txt") as input:
+        supports_ssl = 0
+        for line in input.read().splitlines():
+            outside = True
+            outside_aba = False
+            inside_bab = False
+            aba_list = []
+            bab_list = []
+            i = 0
+            x = len(line)
+            while i < x-2:
+                aba = ""
+                bab = ""
+                if line[i] == line[i+2] and line[i+1] != line[i] and outside:
+                    aba += line[i] + line[i+1] + line[i+2]
+                    if "[" not in aba and "]" not in aba:
+                        aba_list.append(aba)
+                    if aba in bab_list:
+                        outside_aba = True
+                if line[i] == line[i+2] and line[i+1] != line[i] and not outside:
+                    bab += line[i+1] + line[i] + line[i+1]
+                    if "[" not in bab and "]" not in bab:
+                        bab_list.append(bab)
+                    if bab in aba_list:
+                        inside_bab = True
+                if line[i] == "[":
+                    outside = False
+                    i += 1
+                    continue
+                if line[i] == "]":
+                    outside = True
+                    i += 1
+                    continue
                 i += 1
-                continue
-
-            if line[i]=="]":
-                outside=True
-                i += 1
-                continue
-            i += 1
-        if inside_bab | outside_aba:
-            supports_ssl += 1
-    print (supports_ssl)
+            if inside_bab | outside_aba:
+                supports_ssl += 1
+        print(supports_ssl)
 
 
 if __name__ == '__main__':
